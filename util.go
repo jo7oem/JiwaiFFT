2df325f729b154c55ef335ca186c8b0907bac767
jo7oem <jo7oem@gmail.com>
2020-08-21 17:34:55 +0900

    Add C2F1d tool

--- util.go
+++ util.go
@@ -1,10 +1,21 @@
 package jiwaifft
 
+import "math/cmplx"
+
 func F2C1d(x []float64) []complex128 {
 	lx := len(x)
 	res := make([]complex128, lx, lx)
 	for i := 0; i < lx; i++ {
 		res[i] = complex(x[i], 0)
+	}
+	return res
+}
+
+func C2F1d(x []complex128) []float64 {
+	lx := len(x)
+	res := make([]float64, lx, lx)
+	for i := 0; i < lx; i++ {
+		res[i] = cmplx.Abs(x[i])
 	}
 	return res
 }
